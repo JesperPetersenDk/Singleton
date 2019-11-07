87a5147687723a06822805258fdcbaa14b4bb8a7
Jesper Petersen <info@ordklar.dk>
2019-11-07 08:35:48 +0100

    rettelse

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ class OnlyOne(object):
             OnlyOne.instance.val = arg
     def __getattr__(self, name):
         return getattr(self.instance, name)
+    def __setattr__(self, name, value):
+        return super().__setattr__(self.instance, value)
 
 x = OnlyOne("sausage")
 print(x)
 y = OnlyOne("eggs")
 print(y)
 z = OnlyOne("Spam")
+print("___________________")
+print(x is y is z)
 print(z)
 print(x)
 print(y)
